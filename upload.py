be486f149db0a3f73c4233fccc2918025c4d9576
kunchao322 <chao.k@northeastern.edu>
2021-10-19 23:25:08 -0700

    convert screen output to txt file, with upload.py to push to S3. Issue: file name changed on S3

---
+++ upload.py
@@ -0,0 +1,39 @@
+# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def main():
+
+    s3 = boto3.client('s3')
+    with open("output.txt", "rb") as f:
+        s3.upload_fileobj(f, 'rekogbucket-project2', "OBJECT_NAME")
+
+
+if __name__ == "__main__":
+    main()
